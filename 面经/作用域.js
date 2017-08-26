b5d0dc3f24f61c181403096ffdfa1d60515a8738
598824163 <598824163@qq.com>
2017-08-26 23:11:44 +0800

    作用域坑题

---
+++ 面经/作用域.js
@@ -0,0 +1,33 @@
+/**
+ * Created by 高文林 on 2017/8/26.
+ */
+
+var a = 1;
+function b() {
+    console.log(a);     //undefined
+    var a = 2;
+}
+b();
+
+var c = 1;
+function d() {
+    console.log(c);   //1
+    c = 2;
+}
+d();
+
+var e = 1;
+function f() {
+    console.log(e);     //[function: e]
+    var e = 2;
+    function e() {}
+}
+f();
+
+var g = 1;
+function h() {
+    console.log(g);     //[function: g]
+    g = 2;
+    function g() {}
+}
+h();
