eed86c7af4bc7d03def077e4886a123ef4436d8c
lushi <lushi@xiaoshijie.com>
2017-12-04 16:30:19 +0800

    quickSort

--- 算法/quickSort.js
+++ 算法/quickSort.js
@@ -1,22 +1,22 @@
-function quickSort(arr) {
-    if(arr.length <= 1){
+const quickSort = arr => {
+    if(arr.length <= 1) {
         return arr;
     }
 
-    var index = Math.floor(arr.length / 2);
-    var index_value = arr.splice(index, 1)[0]; //选择基准并删除
-    var left = [], right = [];
+    let left = [], right = [];
+    const index = Math.floor(arr.length / 2);
+    const item_index = arr.splice(index, 1)[0];
 
-    arr.forEach(function(item){
-        if(item < index_value){
-            left.push(item);
-        } else {
-            right.push(item);
-        }
-    });
+    arr.forEach((item) => {
+        if(item < item_index) {
+        left.push(item)
+    } else {
+        right.push(item)
+    }
+})
 
-    return quickSort(left).concat([index_value], quickSort(right));
+    return quickSort(left).concat([item_index], quickSort(right))
 }
 
-var x = [1,3,2,2,5,4];
+const x = [1, 3, 2, 2, 5, 4];
 console.log(quickSort(x));
